a9929a0616bba758d7008f23d61cf21a5d122c36
omtathe <omtathe45@gmail.com>
2026-03-14 14:56:03 +0530

    Update project files

--- qrcode_generator.py
+++
@@ -1,5 +0,0 @@
-import qrcode as qr
-
-img = qr.make("Me hu aditya")
-
-img.save("qr_code.png")
--- qrcode_generator2.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-
-from PIL import Image
-
-qr = qrcode.QRCode(version=1,
-                   error_correction=qrcode.constants.Error_handle_H,
-                   box_size=10,
-                   border=4,)
